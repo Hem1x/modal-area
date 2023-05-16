06621704510efba8f45668a3c20c7e44c4a9d915
Sanek-2004 <iwanov.vany-ivanov@yandex.ru>
2023-05-16 17:23:15 +0300

    modal is done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
   return (
     <div className="App">
-      <button>Open model</button>
+      <button className='open-modal-btn' onClick={() => setIsModalOpen(true)}>Open modal</button>
+      <div className={`overlay animated ${isModalOpen ? 'show' : ''}`}>
+        <div className='modal'>
+          <svg onClick={() => setIsModalOpen(false)} height="200" viewBox="0 0 200 200" width="200">
+                  <title />
+                  <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+          </svg>
+        </div>
+      </div>
     </div>
   );
 }
