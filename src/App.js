3e47d49a53d4d82639ace16bae7570b40ff81a07
Sanek-2004 <iwanov.vany-ivanov@yandex.ru>
2023-05-16 17:29:02 +0300

    lastCommit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './App.css';
+import Modal from './components/Modal';
 
 function App() {
   const [isModalOpen, setIsModalOpen] = useState(false)
@@ -7,14 +8,11 @@ function App() {
   return (
     <div className="App">
       <button className='open-modal-btn' onClick={() => setIsModalOpen(true)}>Open modal</button>
-      <div className={`overlay animated ${isModalOpen ? 'show' : ''}`}>
-        <div className='modal'>
-          <svg onClick={() => setIsModalOpen(false)} height="200" viewBox="0 0 200 200" width="200">
-                  <title />
-                  <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-          </svg>
-        </div>
-      </div>
+      <Modal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}>
+        <h1>The Earth</h1>
+        <img src='https://www.pnp.ru/upload/entities/2021/04/19/18/article/detailPicture/7e/66/9f/55/23c871532e2289d5791561c8adda1a1a.jpg' alt='earth'/>
+        <p>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века. В то время некий безымянный печатник создал большую коллекцию размеров и форм шрифтов, используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не только успешно пережил без заметных изменений пять веков, но и перешагнул в электронный дизайн. Его популяризации в новое время послужили публикация листов Letraset с образцами Lorem Ipsum в 60-х годах и, в более недавнее время, программы электронной вёрстки типа Aldus PageMaker, в шаблонах которых используется Lorem Ipsum.</p>
+      </Modal>
     </div>
   );
 }
