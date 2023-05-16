7455b600363a73abcbb6f603556de19a9d3f2cf6
Sanek-2004 <iwanov.vany-ivanov@yandex.ru>
2023-05-16 16:50:25 +0300

    Component Model created without logic

---
+++ src/components/Model.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Model = () => {
+  return (
+    <div>
+        <div className="overlay">
+            <div className="model">
+                <svg onClick={() => setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
+                    <title />
+                    <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+                </svg>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Model
